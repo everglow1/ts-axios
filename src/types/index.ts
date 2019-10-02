b94a427280260d5c133b2e44e05c7e47e0a47017
weidian <youzhijun@weidian.com>
2019-10-02 21:44:39 +0800

    简单的xhr请求

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,10 @@
+// 字符串字面量类型
 export type Method = 'get' | 'GET' | 'delete' | 'Delete' | 'head' | 'HEAD' | 'options' | 'OPTIONS' | 'post' | 'POST' | 'put' | 'PUT' | 'patch' | 'PATCH'
 
+// 请求配置接口
 export interface AxiosRequestConfig {
 	url: string
-	methods?: Method
+	method?: Method
 	data?: any   // post请求使用
 	params?: any
 }
